3896217d808d0908b4537f40188e4d62754e9512
tharunamireddy <tharunamireddy07@gmail.com>
2024-09-08 02:17:14 +0530

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/customers';
+const API_URL = 'https://cms-server-7gd8.onrender.com/api/customers';
 
 export const getAllCustomers = async () => {
   try {
